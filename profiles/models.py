9de66934abb0314daec5577a2dad3096b1b0a71d
NDOMINEY <>
2023-09-06 16:37:19 +0000

    Add profile model and add to admin
     - Create Profile Model
    - Add created on sorting
    - Add auto create when user is added

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200, blank=True)
+    about = models.CharField(max_length=500, blank=True)
+    picture = models.ImageField(
+        upload_to='images/', default='default_profile_igf8vw')
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f"{self.name}'s profile"
+
+
+def create_profile(sender, instance, created, **Kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
