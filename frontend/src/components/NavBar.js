f196a205e890cd8ed4d588922328a52965da3cb1
NDOMINEY <>
2023-09-14 08:07:20 +0000

    Add React Bootstrap and create generic NavBar to test

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="">The Pedal Platform</Navbar.Brand>
+            <Nav className="ml-auto">
+                <Nav.Link href="">Home</Nav.Link>
+                <Nav.Link href="">Products</Nav.Link>
+                <Nav.Link href="">Login</Nav.Link>
+            </Nav>
+        </Navbar>
     );
 };
 
