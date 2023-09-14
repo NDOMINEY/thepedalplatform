e339da8f7798f21926180eefc02800830361b3c6
NDOMINEY <>
2023-09-14 21:52:52 +0000

    Add react routing and set up basic page switching
    - install react-router-dom@5.3.0
    - Add Navlinks to NavBar
    - Add Switch and Route to App.js
    - Tested switch locally

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import logo_short_transparent from '../assets/logo_short_transparent.png';
 import styles from '../styles/NavBar.module.css';
 
 
 const NavBar = () => {
     return (
-        <Navbar className={styles.NavBar} expand="lg">
-            <Navbar.Brand href="#">
-                <img src={logo_short_transparent} alt='logo' height="50" />
-            </Navbar.Brand>
+        <Navbar className={styles.NavBar} expand="lg" fixed="top">
+            <NavLink to="/">
+                <Navbar.Brand>
+                    <img src={logo_short_transparent} alt='logo' height="50" />
+                </Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
-                <Nav className="ml-auto text-right links">
-                    <Nav.Link href="">Home</Nav.Link>
-                    <Nav.Link href="">Products</Nav.Link>
-                    <Nav.Link href="">Login</Nav.Link>
-                    <Nav.Link href="">Register</Nav.Link>
+                <Nav className="ml-auto text-right">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/products">Products</NavLink>
+                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/register">Register</NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
