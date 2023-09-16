f980a67b8fd7390cc70c6e2f097d55e664607c35
NDOMINEY <>
2023-09-16 11:05:48 +0000

    Add transparent logo and include in landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Landing from './components/Landing';
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 
@@ -10,7 +11,9 @@ function App() {
             <NavBar />
             <Container className={styles.Content}>
                 <Switch>
-                    <Route exact path="/" render={() => <h1>Home page</h1>} />
+                    <Route exact path="/" render={() =>
+                        <Landing />
+                    } />
                     <Route exact path="/products" render={() => <h1>Products</h1>} />
                     <Route exact path="/login" render={() => <h1>Login</h1>} />
                     <Route exact path="/register" render={() => <h1>Register</h1>} />
---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo_transparent from '../assets/logo_transparent.png';
+
+const Landing = () => {
+    return (
+        <div>
+            <img src={logo_transparent} alt="logo" />
+        </div>
+    );
+};
+
+export default Landing;
