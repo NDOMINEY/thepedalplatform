80ee34067d5fde4e65336db9ddd4e59fa94e9f90
NDOMINEY <>
2023-09-16 16:29:20 +0000

    Add basic register form structure without styling

--- frontend/src/pages/RegisterForm.js
+++ frontend/src/pages/RegisterForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "react-bootstrap";
+import { Col, Row, Container, Form, Button } from "react-bootstrap";
 
 const RegisterForm = () => {
     return (
@@ -8,6 +8,31 @@ const RegisterForm = () => {
             <Col className="my-auto py-2 p-md-2">
                 <Container>
                     <h1>Register</h1>
+
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formGroupEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password1">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                name="password1" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password2">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Confirm Password"
+                                name="password2" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+
                 </Container>
                 <Container>
                     <Link to="/login">
