0578cbee8148ae39ff089480dbbc3f98dead8807
NDOMINEY <>
2023-09-17 15:33:47 +0000

    Add login form and tested functionality
    - Add LoginForm.js and create basic login form structure
    - Add handleSubmit
    - Add to app.js /login
    - Tested login function but to work cookies must be cleared each time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import LoginForm from './pages/LoginForm';
 import RegisterForm from './pages/RegisterForm';
 import Landing from './components/Landing';
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
 
 
 function App() {
@@ -16,7 +18,7 @@ function App() {
                         <Landing />
                     } />
                     <Route exact path="/products" render={() => <h1>Products</h1>} />
-                    <Route exact path="/login" render={() => <h1>Login</h1>} />
+                    <Route exact path="/login" render={() => <LoginForm />} />
                     <Route exact path="/register" render={() => <RegisterForm />} />
                     <Route render={() => <p>Page not found!</p>} />
                 </Switch>
---
+++ frontend/src/pages/LoginForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Col, Row, Container, Form, Button, Alert } from "react-bootstrap";
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: '',
+    });
+    const { username, password } = loginData;
+
+    const handleChange = (event) => setLoginData({
+        ...loginData,
+        [event.target.name]: event.target.value
+    });
+
+    const [errors, setErrors] = useState({});
+    const history = useHistory();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/login/", loginData);
+            history.push("/");
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    };
+
+    return (
+        <Row>
+            <Col className="my-auto py-2 p-md-2">
+                <Container>
+                    <h1>Login</h1>
+
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="username">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                name='username'
+                                value={username}
+                                onChange={handleChange} />
+                        </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                value={password}
+                                onChange={handleChange} />
+                        </Form.Group>
+                        {errors.password?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+                    </Form>
+
+                </Container>
+                <Container>
+                    <Link to="/register">
+                        Don't have an account? Register
+                    </Link>
+                </Container>
+            </Col>
+        </Row>
+    );
+};
+
+export default LoginForm;
