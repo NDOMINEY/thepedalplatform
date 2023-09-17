95ec5c253672b5e24b41e389c382f7668b576991
NDOMINEY <>
2023-09-17 16:34:47 +0000

    Seperate CurrentUserContext from app.js and import to index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,29 +5,11 @@ import RegisterForm from './pages/RegisterForm';
 import Landing from './components/Landing';
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import { createContext, useEffect, useState } from "react";
-import axios from 'axios';
+import "./api/axiosDefaults";
 
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 
 function App() {
-    const [currentUser, setCurrentUser] = useState(null);
-
-    const handleMount = async () => {
-        try {
-            const { data } = await axios.get("dj-rest-auth/user/");
-            setCurrentUser(data);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    useEffect(() => {
-        handleMount();
-    }, []);
-
     return (
         <CurrentUserContext.Provider value={currentUser}>
             <SetCurrentUserContext.Provider value={setCurrentUser}>
