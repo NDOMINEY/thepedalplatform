ec20a278f4fd977f4ea5b62a218dd5fe8d32ee2f
NDOMINEY <>
2023-09-21 21:15:39 +0000

    Add pedal model and add to admin.py

--- pedals/admin.py
+++ pedals/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Brands
+from .models import Brands, Pedal
 
 # Register your models here.
 
 admin.site.register(Brands)
+admin.site.register(Pedal)
--- pedals/models.py
+++ pedals/models.py
@@ -2,6 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+CATEGORY = (
+    ("CHORUS", "Chorus"),
+    ("LOOPER", "Looper"),
+    ("OCTAVE", "Octave"),
+    ("FUZZ", "Fuzz"),
+    ("OVERDRIVE AND DISTORTION", "Overdrive and Distortion"),
+    ("COMPRESSOR", "Compressor")
+)
+
+
+PRICE = (
+    ("£", "£"),
+    ("££", "££"),
+    ("£££", "£££"),
+    ("££££", "££££"),
+    ("£££££", "£££££")
+)
+
 
 class Brands(models.Model):
     brand = models.CharField(max_length=250, blank=True)
@@ -13,3 +31,25 @@ class Brands(models.Model):
 
     def __str__(self):
         return f"{self.brand}"
+
+
+class Pedal(models.Model):
+    brand = models.OneToOneField(Brands, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True)
+    category = models.CharField(max_length=50,
+        choices=CATEGORY,
+        default='£'
+    )
+    price = models.CharField(
+        max_length=5,
+        choices=PRICE,
+        default='£'
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-name']
+
+    def __str__(self):
+        return f"{self.name}"
