dfee64f1607e76ba1ce1e7c5cf50b6db68a115fa
NDOMINEY <>
2023-09-22 20:52:54 +0000

    Remove debugging logs

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,19 +1,17 @@
 import React, { useContext } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+
+
 import logo_short_transparent from '../assets/logo_short_transparent.png';
 import styles from '../styles/NavBar.module.css';
 import axios from 'axios';
 import { SetCurrentUserContext, CurrentUserContext } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
+
     const currentUser = useContext(CurrentUserContext);
     const setCurrentUser = useContext(SetCurrentUserContext);
-
-
-    console.log('Check 1', currentUser);
-
-    console.log('Check 2', useContext(CurrentUserContext));
 
     const handleLogout = async () => {
         try {
@@ -36,7 +34,6 @@ const NavBar = () => {
             <NavLink to="/register">Register</NavLink>
         </>
     );
-
 
     return (
         <Navbar className={styles.NavBar} expand="lg" fixed="top">
