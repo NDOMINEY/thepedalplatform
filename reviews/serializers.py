9dca38f646922f656b1f361dca8f291ebc4d2b04
NDOMINEY <>
2023-09-25 21:05:28 +0000

    Add formatting to reviews datetimefield to display ddmmyy

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -4,6 +4,8 @@ from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
     pedal = serializers.ReadOnlyField(source='pedal.id')
+    owner = serializers.ReadOnlyField(source='owner.username')
+    created_at = serializers.DateTimeField(read_only=True, format="%d-%m-%Y")
 
     class Meta:
         model = Review
