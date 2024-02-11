28523eec2a73624eea9c88530b8f5b6a542daa6f
NDOMINEY <natalie.r.dominey@gmail.com>
2024-02-11 22:46:31 +0000

    Add product display component to show favourite pedals
    - tested locally

---
+++ frontend/src/components/ProductDisplay.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import styles from "../styles/ProductDetail.module.css";
+import { axiosReq } from "../api/axiosDefaults";
+import { Link } from "react-router-dom";
+import loading from "../assets/loading.gif";
+
+function ProductDisplay(props) {
+  const id = props.id;
+
+  const [pedalDetail, setPedal] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: pedalDetail }] = await Promise.all([
+          axiosReq.get(`/pedal/${id}`),
+        ]);
+
+        setPedal(pedalDetail);
+
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setHasLoaded(false);
+    handleMount();
+  }, [id]);
+
+  return (
+    <div>
+      <section className={styles.product_list}>
+        {hasLoaded ? (
+          <Link to={`/products/${pedalDetail.id}`} key={id}>
+            <div className={styles.product_items}>
+              <h6 className={styles.name_color}>{pedalDetail.name}</h6>
+              <h6 className={styles.brand_color}>{pedalDetail.brand}</h6>
+              <h6 className={styles.price_color}>{pedalDetail.price}</h6>
+
+              <div className={styles.pedal_light}></div>
+
+              <div>
+                <button className={styles.pedal_button}></button>
+              </div>
+            </div>
+          </Link>
+        ) : (
+          <div className={styles.product_items}>
+            <p className={styles.loading_text}>Loading</p>
+            <img src={loading} alt="loading"></img>
+          </div>
+        )}
+      </section>
+    </div>
+  );
+}
+
+export default ProductDisplay;
--- frontend/src/pages/Favourites.js
+++ frontend/src/pages/Favourites.js
@@ -1,25 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { axiosReq } from "../api/axiosDefaults";
+import loading from "../assets/loading.gif";
+import ProductDisplay from "../components/ProductDisplay";
 
 const Favourite = () => {
   const { id } = useParams();
+
   const [hasLoaded, setHasLoaded] = useState(false);
+  const [favouriteData, setFavouriteData] = useState({ results: [] });
 
   useEffect(() => {
-    const fetchProfile = async () => {
+    const handleMount = async () => {
       try {
-        const { data } = await axiosReq.get(`/favourite/?owner=${id}`);
+        const { data: favourites } = await axiosReq.get(
+          `/favourite/?owner=${id}`
+        );
+        setFavouriteData(favourites);
         setHasLoaded(true);
       } catch (err) {
         console.log(err);
       }
     };
     setHasLoaded(false);
-    fetchProfile();
+    handleMount();
   }, [id]);
 
-  return <></>;
+  return (
+    <div>
+      <section>
+        {hasLoaded ? (
+          favouriteData.length ? (
+            favouriteData.map((favourite) => (
+              <ProductDisplay id={favourite.pedal} />
+            ))
+          ) : (
+            <div>
+              <p>No Favourites</p>
+            </div>
+          )
+        ) : (
+          <div>
+            <p>Loading</p>
+            <img src={loading} alt="loading"></img>
+          </div>
+        )}
+      </section>
+    </div>
+  );
 };
 
 export default Favourite;
