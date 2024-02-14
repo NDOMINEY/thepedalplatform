fe2ffeee42d50c0ed8c8915aba74167d761ed46c
NDOMINEY <natalie.r.dominey@gmail.com>
2024-02-14 20:47:50 +0000

    Add styling to login and register forms

--- frontend/src/pages/LoginForm.js
+++ frontend/src/pages/LoginForm.js
@@ -1,94 +1,94 @@
 import React, { useContext, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Col, Row, Container, Form, Button, Alert } from "react-bootstrap";
-import axios from 'axios';
-import styles from '../styles/ProductDetail.module.css';
+import axios from "axios";
+import stylesBTN from "../styles/ProductDetail.module.css";
+import styles from "../styles/LoginRegister.module.css";
 
 import { SetCurrentUserContext } from "../contexts/CurrentUserContext";
 
-
 const LoginForm = () => {
-    const setCurrentUser = useContext(SetCurrentUserContext);
+  const setCurrentUser = useContext(SetCurrentUserContext);
 
-    const [loginData, setLoginData] = useState({
-        username: '',
-        password: '',
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = loginData;
+
+  const handleChange = (event) =>
+    setLoginData({
+      ...loginData,
+      [event.target.name]: event.target.value,
     });
-    const { username, password } = loginData;
 
-    const handleChange = (event) => setLoginData({
-        ...loginData,
-        [event.target.name]: event.target.value
-    });
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
 
-    const [errors, setErrors] = useState({});
-    const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", loginData);
+      setCurrentUser(data.user);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const { data } = await axios.post("/dj-rest-auth/login/", loginData);
-            setCurrentUser(data.user);
-            history.push("/");
-        } catch (err) {
-            setErrors(err.response?.data);
-        }
-    };
+  return (
+    <Row className={styles.login_register_container}>
+      <Col className="my-auto py-2 p-md-2">
+        <Container>
+          <h1>Login</h1>
 
-    return (
-        <Row>
-            <Col className="my-auto py-2 p-md-2">
-                <Container>
-                    <h1>Login</h1>
-
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Enter username"
-                                name='username'
-                                value={username}
-                                onChange={handleChange} />
-                        </Form.Group>
-                        {errors.username?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                        <Form.Group className="mb-3" controlId="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Password"
-                                name="password"
-                                value={password}
-                                onChange={handleChange} />
-                        </Form.Group>
-                        {errors.password?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                        <Button className={styles.btn_review} type="submit">
-                            Login
-                        </Button>
-                        {errors.non_field_errors?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                    </Form>
-
-                </Container>
-                <Container>
-                    <Link to="/register">
-                        Don't have an account? Register
-                    </Link>
-                </Container>
-            </Col>
-        </Row>
-    );
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Button className={stylesBTN.btn_review} type="submit">
+              Login
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container>
+          <Link to="/register">Don't have an account? Register</Link>
+        </Container>
+      </Col>
+    </Row>
+  );
 };
 
 export default LoginForm;
--- frontend/src/pages/RegisterForm.js
+++ frontend/src/pages/RegisterForm.js
@@ -1,104 +1,109 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Col, Row, Container, Form, Button, Alert } from "react-bootstrap";
-import axios from 'axios';
-import styles from '../styles/ProductDetail.module.css';
-
+import axios from "axios";
+import stylesBTN from "../styles/ProductDetail.module.css";
+import styles from "../styles/LoginRegister.module.css";
 
 const RegisterForm = () => {
-    const [registerData, setRegisterData] = useState({
-        username: '',
-        password1: '',
-        password2: '',
+  const [registerData, setRegisterData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = registerData;
+
+  const handleChange = (event) =>
+    setRegisterData({
+      ...registerData,
+      [event.target.name]: event.target.value,
     });
-    const { username, password1, password2 } = registerData;
 
-    const handleChange = (event) => setRegisterData({
-        ...registerData,
-        [event.target.name]: event.target.value
-    });
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
 
-    const [errors, setErrors] = useState({});
-    const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", registerData);
+      history.push("/login");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            await axios.post("/dj-rest-auth/registration/", registerData);
-            history.push("/login");
-        } catch (err) {
-            setErrors(err.response?.data);
-        }
-    };
+  return (
+    <Row className={styles.login_register_container}>
+      <Col className="my-auto py-2 p-md-2">
+        <Container>
+          <h1>Register</h1>
 
-    return (
-        <Row>
-            <Col className="my-auto py-2 p-md-2">
-                <Container>
-                    <h1>Register</h1>
-
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="username">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="Enter email"
-                                name='username'
-                                value={username}
-                                onChange={handleChange} />
-                        </Form.Group>
-                        {errors.username?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                        <Form.Group className="mb-3" controlId="password1">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Password"
-                                name="password1"
-                                value={password1}
-                                onChange={handleChange} />
-                        </Form.Group>
-                        {errors.password1?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                        <Form.Group className="mb-3" controlId="password2">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Confirm Password"
-                                name="password2"
-                                value={password2}
-                                onChange={handleChange} />
-                        </Form.Group>
-                        {errors.password2?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                        <Button className={styles.btn_review} variant="primary" type="submit">
-                            Submit
-                        </Button>
-                        {errors.non_field_errors?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                    </Form>
-
-                </Container>
-                <Container>
-                    <Link to="/login">
-                        Already have an account? Sign in
-                    </Link>
-                </Container>
-            </Col>
-        </Row>
-    );
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Form.Group className="mb-3" controlId="password1">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password1"
+                value={password1}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password1?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Form.Group className="mb-3" controlId="password2">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+                value={password2}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password2?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Button
+              className={stylesBTN.btn_review}
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container>
+          <Link to="/login">Already have an account? Sign in</Link>
+        </Container>
+      </Col>
+    </Row>
+  );
 };
 
 export default RegisterForm;
