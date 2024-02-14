39c99408b4a2f1a902fd13ecf167129f515d61b8
NDOMINEY <natalie.r.dominey@gmail.com>
2024-02-14 20:41:03 +0000

    Add styling for landing page and basic content - add links to sign posted areas of site

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import logo_transparent from '../assets/logo_transparent.png';
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import logo_transparent from "../assets/logo_transparent.png";
+import styles from "../styles/Landing.module.css";
 
 const Landing = () => {
-    return (
-        <div>
-            <img src={logo_transparent} alt="logo" />
-        </div>
-    );
+  const currentUser = useContext(CurrentUserContext);
+
+  return (
+    <div className={styles.landing_container}>
+      <h1>WELCOME TO</h1>
+      <img src={logo_transparent} alt="logo" />
+      <br />
+      <br />
+
+      <h5>
+        A community for pedal enthusiasts to leave reviews, comments and keep a
+        list of your favourite pedals!
+      </h5>
+      <br />
+      <br />
+
+      <h6>
+        Take a look at our pedal inventory here &#9758;
+        <Link to="/products" className={styles.links}>
+          &#9783;
+        </Link>
+      </h6>
+      <br />
+      {!currentUser ? (
+        <>
+          <h6>
+            Login here &#9758;
+            <Link to="/login" className={styles.links}>
+              &#10162;
+            </Link>
+          </h6>
+          <h6>
+            Register here &#9758;
+            <Link to="/register" className={styles.links}>
+              &#9998;
+            </Link>
+          </h6>
+        </>
+      ) : null}
+      <br />
+    </div>
+  );
 };
 
 export default Landing;
